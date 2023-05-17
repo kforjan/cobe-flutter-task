1416789458d011133dab32431321a8c1622e4216
Krešimir Forjan <kresimirforjan5@gmail.com>
2023-05-17 20:09:52 +0200

    add route names and paths

---
+++ lib/common/constants/route_names.dart
@@ -0,0 +1,6 @@
+abstract class RouteNames {
+  static const splash = 'SplashPage';
+  static const home = 'HomePage';
+  static const favorites = 'FavoritesPage';
+  static const details = 'DetailsPage';
+}
---
+++ lib/common/constants/route_paths.dart
@@ -0,0 +1,6 @@
+abstract class RoutePaths {
+  static const splash = '/splash';
+  static const home = '/home';
+  static const favorites = '/favorites';
+  static const details = '/details';
+}
