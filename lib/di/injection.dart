4c5da7107749a9023bdbf9427ec605b7995f65bb
Krešimir Forjan <kresimirforjan5@gmail.com>
2023-05-17 19:55:58 +0200

    create injection container

---
+++ lib/di/injection.dart
@@ -0,0 +1,10 @@
+import 'package:cobe_flutter_task/di/injection.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit(
+  preferRelativeImports: true,
+)
+void configureDependencies() => getIt.init();
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:cobe_flutter_task/app.dart';
+import 'package:cobe_flutter_task/di/injection.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  configureDependencies();
 
   runApp(const App());
 }
