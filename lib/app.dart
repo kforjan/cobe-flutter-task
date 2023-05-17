1e36d287a3c98fe88b7c9c18206c887b088cb980
Krešimir Forjan <kresimirforjan5@gmail.com>
2023-05-17 20:25:45 +0200

    add l10n and generate files

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:cobe_flutter_task/di/injection.dart';
+import 'package:cobe_flutter_task/generated/l10n.dart';
 import 'package:cobe_flutter_task/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,6 +11,14 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: getIt<AppRouter>().routerConfig,
+      onGenerateTitle: (context) => S.of(context).app_title,
+      supportedLocales: S.delegate.supportedLocales,
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
     );
   }
 }
