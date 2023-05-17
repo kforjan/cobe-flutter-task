ef69406ddbc8ecd0abcacc78d45b78dad3a0396a
Krešimir Forjan <kresimirforjan5@gmail.com>
2023-05-17 20:19:30 +0200

    add navigation module with gorouter and add routes

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -10,7 +10,15 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
+import 'package:go_router/go_router.dart' as _i4;
 import 'package:injectable/injectable.dart' as _i2;
+
+import '../config/flavor_config.dart' as _i3;
+import '../router/app_router.dart' as _i5;
+import 'modules/navigation_module.dart' as _i6;
+
+const String _dev = 'dev';
+const String _prod = 'prod';
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -23,6 +31,19 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
+    final navigationModule = _$NavigationModule();
+    gh.singleton<_i3.FlavorConfig>(
+      _i3.DevConfig(),
+      registerFor: {_dev},
+    );
+    gh.singleton<_i3.FlavorConfig>(
+      _i3.ProdConfig(),
+      registerFor: {_prod},
+    );
+    gh.singleton<_i4.GoRouter>(navigationModule.goRouter);
+    gh.singleton<_i5.AppRouter>(_i5.AppRouter(gh<_i4.GoRouter>()));
     return this;
   }
 }
+
+class _$NavigationModule extends _i6.NavigationModule {}
---
+++ lib/di/modules/navigation_module.dart
@@ -0,0 +1,13 @@
+import 'package:cobe_flutter_task/common/constants/route_paths.dart';
+import 'package:cobe_flutter_task/router/routes.dart';
+import 'package:go_router/go_router.dart';
+import 'package:injectable/injectable.dart';
+
+@module
+abstract class NavigationModule {
+  @singleton
+  GoRouter get goRouter => GoRouter(
+        routes: Routes.routes,
+        initialLocation: RoutePaths.splash,
+      );
+}
