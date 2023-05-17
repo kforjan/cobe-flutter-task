e29746aa407c632e57141fd0fdfa5927ee41a1ca
Krešimir Forjan <kresimirforjan5@gmail.com>
2023-05-17 19:53:01 +0200

    master

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:cobe_flutter_task/app.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const App());
+}
