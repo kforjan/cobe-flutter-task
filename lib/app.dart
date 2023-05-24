a74ce0758a8700351e9b1fc02d738a49d798859a
Krešimir Forjan <kresimirforjan5@gmail.com>
2023-05-24 16:25:32 +0200

    update main and app

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,11 @@
+import 'package:cobe_flutter_task/common/blocs/connectivity_bloc/connectivity_bloc.dart';
 import 'package:cobe_flutter_task/di/injection.dart';
 import 'package:cobe_flutter_task/generated/l10n.dart';
+import 'package:cobe_flutter_task/presentation/favorites/bloc/favorites_bloc.dart';
+import 'package:cobe_flutter_task/presentation/popular/bloc/popular_bloc.dart';
 import 'package:cobe_flutter_task/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 class App extends StatelessWidget {
@@ -9,16 +13,31 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: getIt<AppRouter>().routerConfig,
-      onGenerateTitle: (context) => S.of(context).app_title,
-      supportedLocales: S.delegate.supportedLocales,
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<PopularBloc>()..add(const PopularEvent.getPopularMovies()),
+        ),
+        BlocProvider(
+          create: (context) =>
+              getIt<FavoritesBloc>()..add(const FavoritesEvent.loadFavorites()),
+        ),
+        BlocProvider(
+          create: (context) => getIt<ConnectivityBloc>(),
+        ),
       ],
+      child: MaterialApp.router(
+        routerConfig: getIt<AppRouter>().routerConfig,
+        onGenerateTitle: (context) => S.of(context).app_title,
+        supportedLocales: S.delegate.supportedLocales,
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:cobe_flutter_task/app.dart';
 import 'package:cobe_flutter_task/di/injection.dart';
 import 'package:cobe_flutter_task/util/environment_reader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  final directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
   configureDependencies(EnvironmentReader.envirnonment);
 
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const App());
 }
+//TODO remove blue scroll drag marker
+// TODO theme
+// TODO check all text colors
